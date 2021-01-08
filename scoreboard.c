6d2137b25257ee02311c0d55de4676fa38adaccf
KUMAR SATYAM <77102131+satyam1511@users.noreply.github.com>
2021-01-08 17:37:50 +0530

    Add files via upload

---
+++ scoreboard.c
@@ -0,0 +1,56 @@
+ #include<stdio.h>
+ #include<conio.h>
+ #include<stdlib.h>
+ void main()
+ {
+ 	 char toss;
+ 	 int s,w,s1,w1,margin;
+ 	 printf("Toss a coin to decide which team play first\n");
+ 	 scanf("%s",&toss);	 
+ 	  if(toss=='t')
+ 	  {
+ 	  	 printf("Coin Tossed");
+ 	   printf("\nTeam A is play first");
+ 	     printf("\nEnter the score and wicket of team A");
+         scanf("%d %d",&s,&w);
+          printf("\nEnter the score and wicket of team B");
+           scanf("%d %d",&s1,&w1);
+        if(s>s1)
+       {
+        	margin=s-s1;
+            printf("\nTeam A is win the game by %d run" ,margin);
+       }
+        else 
+       {
+     	margin=s1-s;
+       printf("\nTeam B is win the game by %d run",margin);
+      }
+    }
+ 	     else if(toss=='h')
+ 	    {
+ 	    	 printf("\nCoin Tossed");
+ 	      printf("\nTeam B is play first");
+ 	        printf("\nEnter the score and wicket of team B");
+             scanf("%d %d",&s,&w);
+           printf("\nEnter the score and wicket of team A");
+           scanf("%d %d",&s1,&w1);
+         if(s>s1)
+         {
+             margin=s-s1;
+            printf("\nTeam B is win the game  by %d", margin);
+         }
+           else
+		   {
+		      margin=s1-s; 
+              printf("\nTeam A is win the game by %d run",margin);
+           }
+        }
+		  else
+		  {
+		    printf("Error occured");
+		    getch();
+		    exit(0);
+          }
+       getch(); 
+ }
+
